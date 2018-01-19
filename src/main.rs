6bf9f8036c698a591293ac8fc16165de3cefaaf7
frk <hazefrk+dev@gmail.com>
2018-01-19 15:31:51 +0100

    Use structopt instead of clap

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,58 @@
 extern crate base64;
-extern crate clap;
 #[macro_use]
 extern crate failure;
 extern crate rust_sodium;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
 
 mod internal;
-
-use clap::{App, Arg};
 use internal::{decrypt_file, encrypt_file, generate_key, transform_key};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "bincrypt", about = "Encrypt a file using XSalsa20-Poly1305!",
+            version = "0.1.1", author = "frk <hazefrk+dev@gmail.com>")]
+struct Opt {
+    /// A flag, true if used in the command line. Enables decryption mode.
+    #[structopt(short = "d", long = "decrypt", help = "Activate decryption mode")]
+    decrypt: bool,
+
+    /// Optional parameter, the output file.
+    #[structopt(short = "o", long = "output", help = "Output file")]
+    output: Option<String>,
+
+    /// Optional parameter, the base64 encoded encryption code.
+    #[structopt(short = "k", long = "key", help = "Base64 encoded encryption key")]
+    key: Option<String>,
+
+    /// Needed parameter, the first on the command line.
+    #[structopt(help = "Input file")]
+    input: String,
+}
 
 fn main() {
-    let matches = App::new("bincrypt")
-        .version("0.1.1")
-        .author("frk <hazefrk+dev@gmail.com>")
-        .about("Encrypt a file using XSalsa20-Poly1305!")
-        .arg(
-            Arg::with_name("file_in")
-                .short("i")
-                .long("input")
-                .value_name("FILE")
-                .help("FILE intput")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("file_out")
-                .short("o")
-                .long("output")
-                .value_name("FILE")
-                .help("FILE output")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("encryption_key")
-                .short("k")
-                .long("key")
-                .value_name("KEY")
-                .help("Base64 encoded encryption key")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("decrypt")
-                .short("d")
-                .long("decrypt")
-                .help("Switch to decrypting"),
-        )
-        .get_matches();
-
-    let key_opt = matches.value_of("encryption_key").and_then(transform_key);
     rust_sodium::init();
 
-    let file_in = matches.value_of("file_in").unwrap();
-    let file_out = matches.value_of("file_out").unwrap();
+    let opt = Opt::from_args();
+    let input = opt.input;
+    let key_opt = opt.key.and_then(|k| transform_key(&k));
 
-    if matches.is_present("decrypt") {
+    if opt.decrypt {
+        let output = opt.output.unwrap_or_else(|| input.clone() + ".dec");
         if let Some(key) = key_opt {
-            if let Err(err) = decrypt_file(file_in, file_out, &key) {
-                println!("Error: {}", err);
+            if let Err(err) = decrypt_file(&input, &output, &key) {
+                eprintln!("Error: {}", err);
             }
         } else {
-            println!("Error: Need valid encryption key to decrypt!")
+            eprintln!("Error: Need valid encryption key to decrypt!")
         }
     } else {
         let key = key_opt.unwrap_or_else(generate_key);
+        let output = opt.output.unwrap_or_else(|| input.clone() + ".enc");
 
-        if let Err(err) = encrypt_file(file_in, file_out, &key) {
-            println!("Error: {}", err);
+        if let Err(err) = encrypt_file(&input, &output, &key) {
+            eprintln!("Error: {}", err);
         }
     }
 }
